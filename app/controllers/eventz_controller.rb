2a096dad3188f52dced7046c4d1e8df02e28b33a
mahichy <mohiuddinoshim@yahoo.com>
2023-12-14 10:34:17 +0600

    Models

--- app/controllers/eventz_controller.rb
+++ app/controllers/eventz_controller.rb
@@ -2,6 +2,7 @@ class EventzController < ApplicationController
 
   def index
     # @age = rand(100)
-    @events = ['BugSmash', 'Hackathon', 'Kata Camp', 'Additional']
+    # @events = ['BugSmash', 'Hackathon', 'Kata Camp', 'Additional']
+    @events = Event.all
   end
 end
