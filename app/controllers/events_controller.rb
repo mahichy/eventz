4e1496fea015ae25f8afd6ecd2419147f9f1aa7c
mahichy <mohiuddinoshim@yahoo.com>
2023-12-20 16:52:30 +0600

    custom queries

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,8 @@ class EventsController < ApplicationController
   def index
     # @age = rand(100)
     # @events = ['BugSmash', 'Hackathon', 'Kata Camp', 'Additional']
-    @events = Event.all
+    # @events = Event.all
+    @events = Event.upcoming
   end
 
   def show
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,10 @@
 class Event < ApplicationRecord
 
+  def self.upcoming
+    where("starts_at > ?", Time.now).order("starts_at desc")
+  end
+
+
   def free?
     price.blank? || price.zero? 
   end
