eeb86728c0976940641c2cf48907e215c6290304
mahichy <mohiuddinoshim@yahoo.com>
2023-12-24 09:41:09 +0600

    One to many:belongs_to

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_20_122228) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_24_032316) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,4 +26,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_20_122228) do
     t.integer "capacity", default: 1
   end
 
+  create_table "registrations", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "how_heard"
+    t.bigint "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_registrations_on_event_id"
+  end
+
+  add_foreign_key "registrations", "events"
 end
