04e3ccdbb291f179c81ce277d7ba89e53cebf3d9
mahichy <mohiuddinoshim@yahoo.com>
2024-01-08 18:48:05 +0600

    through associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
 
   has_many :registrations, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :likers, through: :likes, source: :user 
 
   validates :name, :description, :location, presence: true
   validates :description, length: {minimum: 10}
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :registrations, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :liked_events, through: :likes, source: :event 
 
 
   validates :name, presence: true
