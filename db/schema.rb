7232f08497da791ee2e846d624c674026d30e10b
mahichy <mohiuddinoshim@yahoo.com>
2024-01-09 15:03:43 +0600

    Many to Many with checkboxes

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_08_052723) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_09_083948) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categorizations", force: :cascade do |t|
+    t.bigint "event_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_categorizations_on_category_id"
+    t.index ["event_id"], name: "index_categorizations_on_event_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -53,6 +68,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_08_052723) do
     t.boolean "admin", default: false
   end
 
+  add_foreign_key "categorizations", "categories"
+  add_foreign_key "categorizations", "events"
   add_foreign_key "likes", "events"
   add_foreign_key "likes", "users"
   add_foreign_key "registrations", "events"
